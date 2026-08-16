0f7c057682a332bd2e0eff429871a76266df6ed0
kbilal3940 <kbilal3940@gmail.com>
2026-08-16 20:48:41 +0530

    Added / into base URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -59,4 +59,4 @@ async def unhandled_exception_handler(request: Request, exc: Exception):
     return JSONResponse(status_code=500, content={"detail": "Internal server error."})
 
 
-app.include_router(router, prefix="api")
+app.include_router(router, prefix="/api")
